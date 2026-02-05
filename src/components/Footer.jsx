9ba9d7b4e54baa26a41e14470b48f3654757ef2c
Asadbek Ikromov <30788816+AsadbekProgrammer@users.noreply.github.com>
2026-02-05 16:28:44 +0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,1 +1,16 @@
+import React from 'react';
+import { Apple, Play } from 'lucide-react';
+
+const Footer = () => {
+  return (
+    <>
+      hello
+    </>
+  );
+};
+
+export default Footer;
+
+
+
 
