7b9c8009bc17d5e32a074a1c06d0dda92807ba9f
Asadbek Ikromov <30788816+AsadbekProgrammer@users.noreply.github.com>
2026-02-05 20:03:28 +0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -32,23 +32,13 @@ const Footer = () => {
             </div>
           </div>
     
-          <div className="bg-[#F9FAFB] max-w-8xl rounded-b-2xl mx-auto p-12 flex flex-col md:flex-row justify-between items-start md:items-center gap-8 min-h-[160px]">
-            <div className="space-y-4">
-              <div className="text-2xl font-black text-tp-orange h-8">TimePay</div>
-              <p className="text-gray-400 text-sm max-w-xs leading-relaxed">
-                Toshkent shahri, Yunusobod tumani, Yangi Shahar ko'chasi
-              </p>
-            </div>
-    
-            <div className="text-gray-400 text-sm font-medium">
-              © 2026 TimePay Workforce Ecosystem. Barcha huquqlar himoyalangan.
-            </div>
-          </div>
+          
         </footer>
   );
 };
 
 export default Footer;
+
 
 
 
